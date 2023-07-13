cb25e26b7df352e4052b039ae1b755511715f1b9
Grisha-3108 <Grisha-3108@yandex.ru>
2023-07-13 18:42:14 +0300

    Тихановский многопоточность

--- multithreading/src/main/java/ResourceProcessor.java
+++ multithreading/src/main/java/ResourceProcessor.java
@@ -11,6 +11,7 @@ public class ResourceProcessor {
     }
 
     public  void increase(){
+        //При достижении ресурса в 5 переводим поток в режим ожидиния
         while (r.getRes() >= 5) {
             try {
                 wait();
@@ -20,10 +21,12 @@ public class ResourceProcessor {
             }
         }
         r.setRes(r.getRes() + 1);
+        //Оживляем другой поток
         notify();
     }
 
     public  void decrease(){
+        //При уменьшении ресурса до меньше 1, останавливаем поток
         while (r.getRes() < 1) {
             try {
                 wait();
@@ -33,6 +36,7 @@ public class ResourceProcessor {
             }
         }
         r.setRes(r.getRes() - 1);
+        //Возобновляем другой поток
         notify();
     }
 }
