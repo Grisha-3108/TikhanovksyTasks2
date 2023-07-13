2b3f08b3b4955bdf9162297204a9a40352cfaf19
Grisha-3108 <Grisha-3108@yandex.ru>
2023-07-13 10:29:53 +0300

    Тихановский обобщения

---
+++ Generics/src/main/java/Number.java
@@ -0,0 +1,27 @@
+//Колллекция элементов
+public class Number {
+    protected int num[] = new int[5];
+    //Для получения элемента
+    public int[] getNum() {
+        return num;
+    }
+
+    Number(){
+        //Выполняем инициализацию коллекции
+        for(int i = 1; i <=5; ++i)
+            num[i - 1] = i;
+    }
+
+    //Создадим обобщённый метод для подсчёта чётных элеметов в коллекции
+    //Принимает коллекцию
+    public <T> int count(T collection){
+        int col = 0;
+        for(int i = 0; i < collection.getNum().length; ++i)
+            if()
+        return col;
+    }
+}
+//Наслденики коллекции из массива из 5 элементов
+class Number1{}
+
+class Number2{}
